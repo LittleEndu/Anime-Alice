cd8b15dd2064d5d1c31418af478f80e42fa5bb29
Endrik Tombak <enduslittlecomputer@gmail.com>
2018-07-19 23:55:47 +0300

    Revert format change a little

--- helper/mediums.py
+++ helper/mediums.py
@@ -158,7 +158,7 @@ query ($terms: String) {
             i['description'] = BS(i['description'], "html.parser").text
 
         if results:
-            index = await helper.Asker(ctx, *[f"\t**{i['title']['romaji']}**\n\t\t{i['title']['english'] or ' '}" for i in results])
+            index = await helper.Asker(ctx, *[f"\t{i['title']['romaji']}\n\t\t*{i['title']['english'] or ' '}*" for i in results])
             wanted = results[index]
             await ctx.trigger_typing()
             anilist_scores = [0 for i in range(10)]  # InFuture: Add scores from other sites maybe
