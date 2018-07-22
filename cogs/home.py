5c4fba277e2d92b2d8c1d461b1ef054b9cfb35fd
Endrik Tombak <enduslittlecomputer@gmail.com>
2018-07-22 16:26:09 +0300

    Log even sooner

--- cogs/home.py
+++ cogs/home.py
@@ -1,4 +1,5 @@
 import aiohttp.web
+import asyncio.base_events
 import discord
 
 import alice
@@ -22,16 +23,16 @@ def setup(bot):
     s_routes = aiohttp.web.RouteTableDef()
 
     @s_routes.post("/api/webhooks/dblVote")
-    async def vote_handler(request):
-        assert isinstance(request, aiohttp.web.Request)
+    async def vote_handler(request: aiohttp.web.Request):
         bot: alice.Alice = request.app.bot
+        bot.logger.debug('POST REQUEST')
         if request.headers.get('Authorization') != bot.config.get('vote_webhook_auth'):
             return aiohttp.web.Response(text='Unauthorized', status=401)
         else:
             webhook: discord.Webhook = await bot.get_webhook_info(bot.config.get('vote_webhook_id'))
             jj = await request.json()
-            await webhook.send(f"Recieved vote: {jj}")
-            bot.logger.debug(f"Recieved vote: {jj}")
+            await webhook.send(f"Received vote: {jj}")
+            bot.logger.debug(f"Received vote: {jj}")
             return aiohttp.web.Response(text="Success")
 
     bot.add_cog(Home(bot, s_routes))
