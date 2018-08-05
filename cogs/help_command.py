9d854f054f3fa911dbe3f79371b70353a3aecac5
Endrik Tombak <enduslittlecomputer@gmail.com>
2018-08-05 23:08:10 +0300

    Make commands look nicer

--- cogs/help_command.py
+++ cogs/help_command.py
@@ -8,6 +8,11 @@ import discord
 import psutil
 from discord.ext import commands
 
+def custom_ljust(value:str, length:int):
+    want = length - len(value)
+    if want < 1:
+        return value
+    return " ".join('\u200b' for _ in range(want + 1)) + value
 
 class HelpCommand:
     def __init__(self, bot: alice.Alice):
@@ -66,7 +71,10 @@ class HelpCommand:
         """
         async with self.bot.helper.AppendOrSend(ctx.author) as appender:
             last_cog = ""
-            for i in sorted(self.bot.commands, key=lambda a: a.cog_name + a.name):
+            sorted_commands = sorted(self.bot.commands, key=lambda a: a.cog_name + a.name)
+            command_names = dict()
+            cog_names = []
+            for i in sorted_commands[:]:
                 assert isinstance(i, commands.Command)
                 can_run = False
                 try:
@@ -74,16 +82,32 @@ class HelpCommand:
                 except commands.CheckFailure:
                     pass
                 show = not i.hidden and can_run
-                if show:
-                    if i.cog_name != last_cog:
-                        last_cog = i.cog_name
-                        await appender.append(f"\n```\U0001f916 {last_cog} \U0001f916```")
-                    new_line = "\n"  # Don't delete, used in nested fstring
-                    help_string = i.brief or f'{i.help or ""}'.split(new_line)[0]
-                    prefix = ctx.prefix if len(ctx.prefix) < 5 else ""
-                    await appender.append(
-                        f"**``{prefix}{i.name}`` - **{f'{help_string}' if help_string else ''}\n"
-                    )
+                if not show:
+                    sorted_commands.remove(i)
+            for i in sorted_commands:
+                if i.cog_name != last_cog:
+                    last_cog = i.cog_name
+                    if cog_names:
+                        ll = max(map(len, cog_names))
+                        for name in cog_names:
+                            command_names[name] = custom_ljust(name, ll)
+                    cog_names = []
+                cog_names.append(i.name)
+            ll = max(map(len, cog_names))
+            for name in cog_names:
+                command_names[name] = custom_ljust(name, ll)
+            ctx.bot.logger.debug(command_names)
+            for i in sorted_commands:
+                assert isinstance(i, commands.Command)
+                if i.cog_name != last_cog:
+                    last_cog = i.cog_name
+                    await appender.append(f"\n```\U0001f916 {last_cog} \U0001f916```")
+                new_line = "\n"  # Don't delete, used in nested fstring
+                help_string = i.brief or f'{i.help or ""}'.split(new_line)[0]
+                prefix = ctx.prefix if len(ctx.prefix) < 5 else ""
+                await appender.append(
+                    f"**``{prefix}{command_names[i.name]}`` - **{f'{help_string}' if help_string else ''}\n"
+                )
         if ctx.guild:
             if not await self.bot.helper.react_or_false(ctx, "\U0001f4eb"):
                 await ctx.send("Sent you the commands")
