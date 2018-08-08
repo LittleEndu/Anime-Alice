0e90ca29a85b2ea25c71a452f79440aef96d43b3
Endrik Tombak <enduslittlecomputer@gmail.com>
2018-08-08 20:14:36 +0300

    Good to know

--- cogs/prefixes.py
+++ cogs/prefixes.py
@@ -52,7 +52,7 @@ class Prefixes:
     @commands.command(aliases=['addprefix'])
     async def setprefix(self, ctx: commands.Context, prefix: str):
         """Sets a prefix for the bot in this server"""
-        if not (ctx.author.guild_permissions.administrator or self.bot.is_owner(ctx.author)):
+        if not (ctx.author.guild_permissions.administrator or await self.bot.is_owner(ctx.author)):
             raise commands.CheckFailure("You can't change the prefix")
         if "\n" in prefix:
             await ctx.send('Prefix cannot contain newlines')
@@ -71,7 +71,7 @@ class Prefixes:
     @commands.command(aliases=['deleteprefix'])
     async def removeprefix(self, ctx, prefix: str = None):
         """Removes a prefix for the bot from this server"""
-        if not (ctx.author.guild_permissions.administrator or self.bot.is_owner(ctx.author)):
+        if not (ctx.author.guild_permissions.administrator or await self.bot.is_owner(ctx.author)):
             raise commands.CheckFailure("You can't change the prefix")
 
         if prefix is None:
